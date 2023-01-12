91e2edef1ff3e4120fa839ad3c8a5fb5589b7844
Darkcodelab <deepanchakravarthi434@gmail.com>
2023-01-12 12:34:19 +0530

    initial project setup

---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Airbnb Clone | Home Page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1 className="text-3xl text-center">Airbnb Clone</h1>
+      </main>
+    </>
+  );
+}
